874bf9a69124b26b788533babb5ad1f4d570a627
ghcstking <vlau8050@bths.edu>
2017-01-12 17:51:41 -0500

    button class complete

--- SimonVicki/src/partnerCodeHere/Button.java
+++ SimonVicki/src/partnerCodeHere/Button.java
@@ -1,28 +1,22 @@
 package partnerCodeHere;
-
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
 import gui.components.Action;
 import gui.components.Component;
-import simonVicki.ButtonInterfaceVicki;
 
-public class Button  extends Component implements ButtonInterfaceVicki{
-	
+public class Button extends Component implements ButtonInterfaceVicki {
+
 	private Action action;
-	private Color initColor;
-	private boolean light;
-
+	private Color c;
+	private boolean highlight;
+	private int x;
+	private int y;
+	
 	public Button() {
-		super(0, 0, 700, 700);
-		light = false;
-	}
-
-	@Override
-	public void act() {
-		action.act();
-		
+		super(0, 0, 80, 80);
+		highlight = false;
 	}
 
 	@Override
@@ -31,54 +25,52 @@ public class Button  extends Component implements ButtonInterfaceVicki{
 	}
 
 	@Override
-	public void turnOn() {
-		// TODO Auto-generated method stub
-		
+	public void act() {
+		action.act();
 	}
 
 	@Override
-	public void turnOff() {
-		// TODO Auto-generated method stub
-		
+	public void setAction(Action a) {
+		this.action = a;
 	}
 
 	@Override
-	public void setAction(Action action) {
-		this.action = action;
-		
-	}
-
-	@Override
-	public void setCoords(int x, int y) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setColor(Color c) {
-		this.initColor = c;
-	}
-
-	@Override
-	public Color getColor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void highlight() {
-		light = true;
-		update();
-	}
-
-	public void dim() {
-		initColor = Color.gray;
-		light = false;
-		update();
+	public void setColor(Color i) {
+		this.c = i;
 	}
 
 	@Override
 	public void update(Graphics2D g) {
+		g = clear();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g.setColor(initColor);
+		if(highlight) {
+			g.setColor(this.c);
+			g.fillOval(0, 0, 65, 65);
+			g.setColor(Color.black);
+			g.drawOval(0, 0, 65, 65);
+		}
+		else {
+			g.setColor(Color.gray);
+			g.fillOval(0, 0, 65, 65);
+			g.setColor(Color.black);
+			g.drawOval(0, 0, 65, 65);
+		}
 	}
 
+	@Override
+	public void turnOn() {
+		highlight = true;
+		update();
+	}
+
+	@Override
+	public void turnOff() {
+		highlight = false;
+		update();
+	}
+
+	@Override
+	public Color getColor() {
+		return c;
+	}
 }
