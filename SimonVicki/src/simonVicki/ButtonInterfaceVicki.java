dcb36ff65a39d541f32ef0956e8dbbead30a1df8
ghcstking <vlau8050@bths.edu>
2017-01-11 22:59:25 -0500

    button interface complete

--- SimonVicki/src/simonVicki/ButtonInterfaceVicki.java
+++ SimonVicki/src/simonVicki/ButtonInterfaceVicki.java
@@ -6,10 +6,10 @@ import gui.components.Action;
 import gui.components.Clickable;
 
 public interface ButtonInterfaceVicki extends Clickable {
-	void turnOn();
-	void turnOff();
-	void setAction(Action action);
-	void setCoords(int x, int y);
+	public void turnOn();
+	public void turnOff();
+	public void setAction(Action action);
+	public void setCoords(int x, int y);
+	public void setColor(Color c);
 	public Color getColor();
-	void setColor(Color c);
 }
